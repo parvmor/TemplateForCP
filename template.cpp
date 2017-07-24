50efb9da4f32cac79f3af4cfc16e7b224b93a483
parv <parvmor.pm@gmail.com>
2017-07-24 14:18:17 +0530

    corrected typos

--- template.cpp
+++ template.cpp
@@ -14,6 +14,12 @@ void _R(long long &x){scanf("%lld",&x);}
 void _R(double &x){scanf("%lf",&x);}
 void _R(char &x){scanf("%c",&x);}
 void _R(char *x){scanf("%s",x);}
+template<typename T>
+void _R(vector<T> &x){
+    for(auto it=x.begin();it!=x.end();it++){
+        _R(*it);
+    }
+}
 void R(){}
 template<typename T,typename... K>
 void R(T& head,K&... tail){_R(head);R(tail...);}
@@ -57,8 +63,8 @@ typedef long long lli;
 #define se second
 #define FOR(i,a,b) for(int i=(a);i<=(b);i++)
 #define REP(i,a) FOR(i,0,(int)(a)-1)
-#define FORd(i,a,b) for(int i=(b);i>=(a);i--)
-#define TR(c,it) for(auto i:(c))
+#define FORd(i,a,b) for(int i=(a);i>=(b);i--)
+#define TR(c,it) for(auto it:(c))
 #define all(x) x.begin(), x.end()
 #define uni(x) x.erase(unique(all(x)), x.end())
 #define sqr(x) ((x)*(x))
@@ -72,27 +78,31 @@ const double PI=3.14159265358979323846264338327950288419716939937510582097494459
 template<typename T> inline T gcd(T a,T b) {if(a==0)return b;return gcd(b%a,a);}
 template<typename T> inline void amin(T& x,T y) {if(x>y)x=y;}
 template<typename T> inline void amax(T& x,T y) {if(x<y)x=y;}
-
+template<typename A,typename B>
+class comp{public:bool operator()(const pair<A,B> &a, const pair<A,B> &b){
+if(a.fi!=b.fi)return a.fi<b.fi;
+else return a.fi>b.fi;
+}};
 //global variables
 
 //end global variables
 
 void preprocess(void){
-	return;
+    return;
 }
 
 int main(void){
-	//freopen("output.txt","w",stdout);
-	//freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    //freopen("input.txt","r",stdin);
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.precision(20);
-	preprocess();
-	int teeee;
-	//cin>>teeee;
-	teeee=1;
-	FOR(zeeee,1,teeee){
-		//printf("Case #%d: ",zeeee);
-	}
-	return 0;
+    preprocess();
+    int teeee;
+    //cin>>teeee;
+    teeee=1;
+    FOR(zeeee,1,teeee){
+        //printf("Case #%d: ",zeeee);
+    }
+    return 0;
 }
